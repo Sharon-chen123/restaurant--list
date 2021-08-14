133772ea2675d321be99fd3a1849ef1cec79e6d7
Sharon-chen123 <chenhsiuhui21@gmail.com>
2021-08-14 11:03:15 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const port = 3000
 const exphbs = require('express-handlebars')
 app.use(express.static('public'))
 
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 // get restaurant list in json file
 const restaurantList = require('./restaurant.json')
 
@@ -37,6 +50,8 @@ app.get('/search', (req, res) => {
   )
   res.render('index', { restaurant, style: 'indexStyle.css', keyword })
 })
+
+
 
 // start and listen on the Express server
 app.listen(port, () => {
