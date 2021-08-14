6f3941bd4ff45eb26941600e238d018e03c7494e
Sharon-chen123 <chenhsiuhui21@gmail.com>
2021-08-15 07:13:48 +0800

    feat: add route home page function

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// app.js
 // require packages used in the project
 const express = require('express')
 const app = express()
-const port = 3000
-
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
 const Restaurant = require('./models/restaurant')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+const port = 3000
 
+// 引用 body-parser
+const bodyParser = require('body-parser')
 
-// require handlebars in the project
 // setting static files for bootstrap and css
-const exphbs = require('express-handlebars')
 app.use(express.static('public'))
 
 // get restaurant list in json file
@@ -28,22 +27,29 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // routes setting
 // index page
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results, style: 'indexStyle.css' })
+  Restaurant.find() // 取出 Restaurant model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(restaurant => res.render('index', { restaurant, style: 'indexStyle.css' })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 //show page
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(item => Number(req.params.restaurant_id) === item.id)
-  res.render('show', { restaurant, style: 'showStyle.css' })
+app.get('/restaurants/:restaurant_id/detail', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant, style: 'showStyle.css' }))
+    .catch(error => console.log(error))
 })
 
 // search function in index page
@@ -56,7 +62,52 @@ app.get('/search', (req, res) => {
   res.render('index', { restaurant, style: 'indexStyle.css', keyword })
 })
 
+// for add restaurant page
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new', { style: 'showStyle.css' })
+})
 
+// for show/detail page
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('Detail', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+//edit the page
+app.get('/restaurants/:restaurant_id/edit', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => {
+      res.render('Edit', { restaurant, style: 'showStyle.css' })
+    })
+    .catch(error => console.log(error))
+})
+// for delete function
+app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//for add restaurant info
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const category = req.body.category
+  const rating = req.body.rating
+  const location = req.body.location
+  const phone = req.body.phone
+  const description = req.body.description
+  const image = req.body.image
+  return Restaurant.create({ name, category, rating, location, phone, description, image }) // save to database
+    .then(() => res.redirect('/')) // go back to homepage 
+    .catch(error => console.log(error))
+})
 
 // start and listen on the Express server
 app.listen(port, () => {
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose')
-const Restaurant = require('../todo') 
+const Restaurant = require('../restaurant')
 const RestaurantList = require('../../restaurant.json') // JSON file
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
+  RestaurantList.results.forEach(restaurant => {
+    Restaurant.create({
+      name: restaurant.name,
+      category: restaurant.category,
+      rating: restaurant.rating,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      description: restaurant.description,
+      image: restaurant.image,
+    })
+  })
 })
