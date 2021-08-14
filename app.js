cc5d1088023d49a7184487004d9f40d9e2cf108f
Sharon-chen123 <chenhsiuhui21@gmail.com>
2021-08-14 11:25:35 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -4,15 +4,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose') // 載入 mongoose
+const Restaurant = require('./models/restaurant')
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+
 // require handlebars in the project
 // setting static files for bootstrap and css
 const exphbs = require('express-handlebars')
 app.use(express.static('public'))
 
-
-
 // get restaurant list in json file
 const restaurantList = require('./restaurant.json')
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
